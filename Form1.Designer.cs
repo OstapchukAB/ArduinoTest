f31698fba27771181e85b1ab96324b107f1cd751
OstapchukAB <antbg@yandex.ru>
2022-05-20 12:48:42 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace ArduinoTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.buttonGetComPort = new System.Windows.Forms.Button();
+            this.buttonConnect = new System.Windows.Forms.Button();
+            this.buttonOnLed = new System.Windows.Forms.Button();
+            this.buttonOffLed = new System.Windows.Forms.Button();
+            this.comboBoxCom = new System.Windows.Forms.ComboBox();
+            this.serialPort = new System.IO.Ports.SerialPort(this.components);
+            this.buttonStatus = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonGetComPort
+            // 
+            this.buttonGetComPort.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonGetComPort.Location = new System.Drawing.Point(12, 28);
+            this.buttonGetComPort.Name = "buttonGetComPort";
+            this.buttonGetComPort.Size = new System.Drawing.Size(216, 60);
+            this.buttonGetComPort.TabIndex = 0;
+            this.buttonGetComPort.Text = "Get Com-port";
+            this.buttonGetComPort.UseVisualStyleBackColor = true;
+            this.buttonGetComPort.Click += new System.EventHandler(this.buttonGetComPort_Click);
+            // 
+            // buttonConnect
+            // 
+            this.buttonConnect.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonConnect.Location = new System.Drawing.Point(12, 183);
+            this.buttonConnect.Name = "buttonConnect";
+            this.buttonConnect.Size = new System.Drawing.Size(216, 60);
+            this.buttonConnect.TabIndex = 1;
+            this.buttonConnect.Text = "Connect";
+            this.buttonConnect.UseVisualStyleBackColor = true;
+            this.buttonConnect.Click += new System.EventHandler(this.buttonConnect_Click);
+            // 
+            // buttonOnLed
+            // 
+            this.buttonOnLed.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonOnLed.Location = new System.Drawing.Point(427, 378);
+            this.buttonOnLed.Name = "buttonOnLed";
+            this.buttonOnLed.Size = new System.Drawing.Size(216, 60);
+            this.buttonOnLed.TabIndex = 2;
+            this.buttonOnLed.Text = "ON LED";
+            this.buttonOnLed.UseVisualStyleBackColor = true;
+            this.buttonOnLed.Click += new System.EventHandler(this.buttonOnLed_Click);
+            // 
+            // buttonOffLed
+            // 
+            this.buttonOffLed.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonOffLed.Location = new System.Drawing.Point(427, 462);
+            this.buttonOffLed.Name = "buttonOffLed";
+            this.buttonOffLed.Size = new System.Drawing.Size(216, 60);
+            this.buttonOffLed.TabIndex = 3;
+            this.buttonOffLed.Text = "OFF LED";
+            this.buttonOffLed.UseVisualStyleBackColor = true;
+            this.buttonOffLed.Click += new System.EventHandler(this.buttonOffLed_Click);
+            // 
+            // comboBoxCom
+            // 
+            this.comboBoxCom.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxCom.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.comboBoxCom.FormattingEnabled = true;
+            this.comboBoxCom.Location = new System.Drawing.Point(12, 113);
+            this.comboBoxCom.Name = "comboBoxCom";
+            this.comboBoxCom.Size = new System.Drawing.Size(216, 32);
+            this.comboBoxCom.TabIndex = 4;
+            // 
+            // buttonStatus
+            // 
+            this.buttonStatus.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonStatus.Location = new System.Drawing.Point(319, 12);
+            this.buttonStatus.Name = "buttonStatus";
+            this.buttonStatus.Size = new System.Drawing.Size(324, 27);
+            this.buttonStatus.TabIndex = 5;
+            this.buttonStatus.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(674, 534);
+            this.Controls.Add(this.buttonStatus);
+            this.Controls.Add(this.comboBoxCom);
+            this.Controls.Add(this.buttonOffLed);
+            this.Controls.Add(this.buttonOnLed);
+            this.Controls.Add(this.buttonConnect);
+            this.Controls.Add(this.buttonGetComPort);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonGetComPort;
+        private System.Windows.Forms.Button buttonConnect;
+        private System.Windows.Forms.Button buttonOnLed;
+        private System.Windows.Forms.Button buttonOffLed;
+        private System.Windows.Forms.ComboBox comboBoxCom;
+        private System.IO.Ports.SerialPort serialPort;
+        private System.Windows.Forms.Button buttonStatus;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ArduinoTest
+{
+    public partial class Form1 : Form
+    {
+        bool isConnected = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        void btnStatus() {
+            if (serialPort.IsOpen)
+                this.buttonStatus.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            else
+                buttonStatus.BackColor = default;
+        }
+        
+        private void connectToArduino()
+        {
+            isConnected = true;
+            string selectedPort = comboBoxCom.GetItemText(comboBoxCom.SelectedItem);
+            serialPort.PortName = selectedPort;
+            serialPort.Open();
+            btnStatus();
+            buttonConnect.Text = "Disconnect";
+        }
+
+        private void disconnectFromArduino()
+        {
+            isConnected = false;
+            serialPort.Close();
+            buttonConnect.Text = "Connect";
+            btnStatus();
+
+        }
+
+        private void buttonConnect_Click(object sender, EventArgs e)
+        {
+            if (!isConnected)
+            {
+                connectToArduino();
+            }
+            else
+            {
+                disconnectFromArduino();
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (serialPort.IsOpen) serialPort.Close();
+        }
+
+        private void buttonOnLed_Click(object sender, EventArgs e)
+        {
+            if (isConnected)
+            {
+                serialPort.Write("1");
+            }
+        }
+
+        private void buttonOffLed_Click(object sender, EventArgs e)
+        {
+            if (isConnected)
+            {
+                serialPort.Write("0");
+            }
+        }
+
+        private void buttonGetComPort_Click(object sender, EventArgs e)
+        {
+            comboBoxCom.Items.Clear();
+            // Получаем список COM портов доступных в системе
+            string[] portnames = SerialPort.GetPortNames();
+            // Проверяем есть ли доступные
+            if (portnames.Length == 0)
+            {
+                MessageBox.Show("COM PORT not found");
+            }
+            foreach (string portName in portnames)
+            {
+                //добавляем доступные COM порты в список           
+                comboBoxCom.Items.Add(portName);
+                Console.WriteLine(portnames.Length);
+                if (portnames[0] != null)
+                {
+                    comboBoxCom.SelectedItem = portnames[0];
+                }
+            }
+        }
+    }
+}
+/*
+ Скетч для приёма сообщений.
+
+
+char commandValue; // данные, поступаемые с последовательного порта
+int ledPin = 13; // встроенный светодиод
+
+void setup() {
+  pinMode(ledPin, OUTPUT); // режим на вывод данных
+  Serial.begin(9600);
+}
+
+void loop() {
+  if (Serial.available()) {
+    commandValue = Serial.read();
+  }
+
+  if (commandValue == '1') {
+    digitalWrite(ledPin, HIGH); // включаем светодиод
+  }
+  else {
+    digitalWrite(ledPin, LOW); // в противном случае выключаем
+  }
+  delay(10); // задержка перед следующим чтением данных
+}
+
+ */
